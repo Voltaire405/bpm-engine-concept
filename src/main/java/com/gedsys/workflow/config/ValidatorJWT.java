17c7f0b4030726fc2b78abde00490e624c6fdf8a
voltaire405 <jlma.ing@protonmail.com>
2023-08-25 16:57:30 -0500

    first commit

---
+++ src/main/java/com/gedsys/workflow/config/ValidatorJWT.java
@@ -0,0 +1,61 @@
+package com.gedsys.workflow.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import io.digitalstate.camunda.authentication.jwt.AbstractValidatorJwt;
+import io.digitalstate.camunda.authentication.jwt.ValidatorResultJwt;
+import org.codehaus.groovy.runtime.IOGroovyMethods;
+import org.codehaus.groovy.runtime.StringGroovyMethods;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class ValidatorJWT extends AbstractValidatorJwt {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ValidatorJWT.class);
+
+    private static String jwtSecret;
+
+    @Override
+    public ValidatorResultJwt validateJwt(String encodedCredentials, String jwtSecretPath) {
+        if (!StringGroovyMethods.asBoolean(jwtSecret)) {
+            try {
+                jwtSecret = IOGroovyMethods.getText((new FileInputStream(jwtSecretPath)));
+            } catch (Exception all) {
+                LOG.error("Unable to load jwt secret: " + all.getLocalizedMessage());
+                return ValidatorResultJwt.setValidatorResult(false, null, null, null);
+            }
+        }
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(jwtSecret);
+            JWTVerifier verifier = JWT.require(algorithm).acceptNotBefore(new Date().getTime()).build();
+            DecodedJWT jwt = verifier.verify(encodedCredentials);
+
+            String username = jwt.getClaim("username").asString();
+            List<String> groupIds = jwt.getClaim("groupIds").asList(String.class);
+            List<String> tenantIds = jwt.getClaim("tenantIds").asList(String.class);
+
+            if (!StringGroovyMethods.asBoolean(username)) {
+                LOG.warn("BAD JWT: Missing username");
+                return ValidatorResultJwt.setValidatorResult(false, null, null, null);
+            }
+
+            return ValidatorResultJwt.setValidatorResult(true, username, groupIds, tenantIds);
+
+        } catch (JWTVerificationException exception) {
+            LOG.warn("BAD JWT: " + exception.getLocalizedMessage());
+            return ValidatorResultJwt.setValidatorResult(false, null, null, null);
+        }
+
+    }
+}
